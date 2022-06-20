cd12236a16b0c1ae104e1aa3ed37557d63646d41
Wahyu Ramadhan <ssjrama@gmail.com>
2022-06-20 11:08:49 +0700

    Initial Commit

---
+++ android/app/src/main/kotlin/com/projek3/presensi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.projek3.presensi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/company_data.dart
@@ -0,0 +1,8 @@
+class CompanyData {
+  static String defaultPassword = 'qwertyuiop';
+  static String defaultRole = 'employee';
+  static Map<String, dynamic> office = {
+    'latitude': -6.4350673,
+    'longitude': 108.3736478,
+  };
+}
